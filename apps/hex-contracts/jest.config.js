21442e99a5d90552c8f09c777de8cceb4c2cf163
Carlo Miguel Dy <carlomigueldy@gmail.com>
2022-03-02 14:09:13 +0800

    Attempt setup Hardhat

--- apps/hex-contracts/jest.config.js
+++ apps/hex-contracts/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'contracts',
+  displayName: 'hex-contracts',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -11,5 +11,5 @@ module.exports = {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/contracts',
+  coverageDirectory: '../../coverage/apps/hex-contracts',
 };
