c165c7baa9cf15c4b620f5be2b444db458fc9fac
Jigme Tashi Namgyal <jigmetashinamgyal@Jigmes-MacBook-Pro.local>
2022-03-04 00:30:19 +0600

    Added(): devise, devise jwt and graphql gem

---
+++ backend/db/migrate/20220303175933_devise_create_users.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# Create User table
+class DeviseCreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      ## Database authenticatable
+
+      t.string :wallet_address, null: false, default: ''
+      t.string :encrypted_password, null: false, default: ''
+
+      t.timestamps null: false
+    end
+
+    add_index :users, :wallet_address, unique: true
+  end
+end
