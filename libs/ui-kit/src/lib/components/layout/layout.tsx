da4a202b56a3d36067e839c9d97d60890a2ae467
kzoeps <kzoepa@gmail.com>
2022-03-04 15:18:13 +0600

    feat(ui-kit): create layout component and barrel file

---
+++ libs/ui-kit/src/lib/components/layout/layout.tsx
@@ -0,0 +1,14 @@
+import './layout.module.scss';
+
+/* eslint-disable-next-line */
+export interface LayoutProps {}
+
+export function Layout(props: LayoutProps) {
+  return (
+    <div>
+      <h1>Welcome to Layout!</h1>
+    </div>
+  );
+}
+
+export default Layout;
