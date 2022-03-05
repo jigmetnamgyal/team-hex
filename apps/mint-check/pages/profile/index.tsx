e961174b52cc2656a1ffb8c34eec4aa1b30bb320
kzoeps <kzoepa@gmail.com>
2022-03-05 19:03:05 +0600

    feat(profile): create page for profile and also component for wallet list

---
+++ apps/mint-check/pages/profile/index.tsx
@@ -0,0 +1,14 @@
+import './profile.module.scss';
+
+/* eslint-disable-next-line */
+export interface ProfileProps {}
+
+export function Index(props: ProfileProps) {
+  return (
+    <div>
+      <h1>Welcome to Profile!</h1>
+    </div>
+  );
+}
+
+export default Index;
---
+++ libs/ui-kit/src/lib/components/wallet-list/wallet-list.tsx
@@ -0,0 +1,14 @@
+import './wallet-list.module.scss';
+
+/* eslint-disable-next-line */
+export interface WalletListProps {}
+
+export function WalletList(props: WalletListProps) {
+  return (
+    <div>
+      <h1>Welcome to WalletList!</h1>
+    </div>
+  );
+}
+
+export default WalletList;
