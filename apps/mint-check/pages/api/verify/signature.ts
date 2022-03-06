0441223532c092f9d0a8ca35ff0bf25032dcc203
Carlo Miguel Dy <carlomigueldy@gmail.com>
2022-03-06 21:46:16 +0800

    Update

--- apps/mint-check/pages/api/verify/signature.ts
+++ apps/mint-check/pages/api/verify/signature.ts
@@ -1,35 +1,37 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { ethers } from 'ethers'
-
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { ethers } from 'ethers';
 
 /**
-   * Hash: Byte32 message created using solidityKeccak256
-   * address: User wallet address
-   * Signature: Signed message by user (Byte32)
-   * 
-   * @param Req The payload containing Address, Hash and Signature
-   * 
-   * {walletAddress: '0x....', Hash: '0x....', Signature: '0x....'}
-   * 
-   * @returns true if address is successfully recovered from signature @else false
-   * 
-*/ 
-async function Signature (req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
-    const { walletAddress, nonce, signature } = req.body
+ * Hash: Byte32 message created using solidityKeccak256
+ * address: User wallet address
+ * Signature: Signed message by user (Byte32)
+ *
+ * @param Req The payload containing Address, Hash and Signature
+ *
+ * {wallet_address: '0x....', nonce: { value: '0x....', message: 'string' }, signature: '0x....'}
+ *
+ * @returns true if address is successfully recovered from signature @else false
+ *
+ */
+async function Signature(request: NextApiRequest, response: NextApiResponse) {
+  if (request.method === 'POST') {
+    const { wallet_address, nonce, signature } = request.body;
 
-    const recoveredAddress =  ethers.utils.verifyMessage(nonce.message, signature)
-    
-    const bool = (recoveredAddress.toLowerCase() === walletAddress.toLowerCase())
-    
-    res.status(200).json(bool)
+    const recoveredAddress = ethers.utils.verifyMessage(
+      nonce.message,
+      signature,
+    );
+
+    const bool =
+      recoveredAddress?.toLowerCase() === wallet_address.toLowerCase();
+
+    response.status(200).json(bool);
   } else {
-    res.status(403).json({
-      response: `${req.method} forbidden`
+    response.status(403).json({
+      response: `${request.method} Forbidden`,
     });
   }
-  
 }
 
 export default Signature;
